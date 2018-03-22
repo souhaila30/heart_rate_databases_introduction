375b641d4193496deaffc8e8fab709190eb0f71e
Souhaila Noor <souhaila@Souhailas-MacBook-Air.local>
2018-03-22 11:17:09 -0400

    get request for heart rate, printer user responses, and get average heart rate

--- web_service.py
+++ web_service.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from main import create_user, add_heart_rate, print_user
+import models
 import numpy as np
 import datetime
 from pymodm import connect
@@ -24,26 +25,48 @@ def heart_rate():
     time = datetime.datetime.now()
     print(email,age, heart_rate)
     try:
-        user = models.User.objects.raw({"_id":email}).first()
         add_heart_rate(email, heart_rate, time)
-        return(email, heart_rate, time)
+        print("User found, responses recorded")
+        return jsonify(email, heart_rate, time)
     except:
         print("user not found, a new user was created")
         create_user(email, age, heart_rate)
-        return(email, age, heart_rate, time)
+        return jsonify(email, age, heart_rate, time)
+
+@app.route("/api/print_user/<user_email>", methods=["GET"])
+def get_print_user(user_email):
+    """print all the information associated with the user email
+    """
+    try:
+        print_user(user_email)
+        return "User responses were printed"
+    except:
+        return "User not found"
 
 @app.route("/api/heart_rate/<user_email>", methods=["GET"])
 def get_all_hr(user_email):
     """returns all heart rate measurements for the user"""
-    all_hr = request.get(user_email)
-    return all_hr
+    try:
+        user = models.User.objects.raw({"_id":user_email}).first()
+        return jsonify("heart rate responses:", user.heart_rate)
+    except:
+        return "User not found"
 
-@app.route("/api/heart_rate/Average/<user_email>", methods=["GET"])
-def get_avg_hr():
-    all_hr = request.get(user_email)
-    num_hr = len(all_hr)
-    avg_hr = [(np.mean((all_hr[i], all_hr[i+1]))) for i in range (len(all_hr)-1)]
-    return avg_hr
+@app.route("/api/heart_rate/average/<user_email>", methods=["GET"])
+def get_avg_hr(user_email):
+    user = models.User.objects.raw({"_id":user_email}).first()
+    print(user.heart_rate)
+    avg_hr = np.mean(user.heart_rate)
+    print(avg_hr)
+    return jsonify("average heart rate is",  avg_hr)
+
+@app.route("/api/heart_rate/interval_average", methods=["POST"])
+def get_avg_hr_interval(user_email, interval):
+    user = models.User.objects.raw({"_id":user_email}).first()
+    r = request.get_json()
+    interval = 
+    max_time = datetime.max(user.user_time)
+    print(max_time)
 
 if __name__ =="__main__":
     app.run(host="127.0.0.1")
