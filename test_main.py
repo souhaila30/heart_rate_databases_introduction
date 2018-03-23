76e2c4ea2c96bda2ba783ba200beb8cad656fb48
Souhaila Noor <souhaila@Souhailas-MacBook-Air.local>
2018-03-23 18:58:23 -0400

    updated

--- test_main.py
+++ test_main.py
@@ -9,6 +9,4 @@ def test_tachycardia():
     assert check_tachycardia(24,60) == "Tachycardia: False"
     assert check_tachycardia(100,112) == "Tachycardia: True"
 
-def test_value_age('san33@duke.edu'):
-    age = return_age(email)
-    asset age == 24
+
