73542f47be26f3e142b84f41a671a43e8b58e2c1
Souhaila Noor <souhaila@Souhailas-MacBook-Air.local>
2018-03-23 21:09:26 -0400

    user input validation

---
+++ validate.py
@@ -0,0 +1,23 @@
+def validate_input_email(POST):
+    """raises a value error if email is not a string"""
+    if isinstance("user_email", str):
+        pass
+    else:
+        raise ValueError "Email must be a string"
+
+def validate_input_hr(POST):
+    """raises value error if heart rate is not a number"""
+    if isinstance("heart_rate", float):
+        pass
+    else:
+        raise ValueError "heart rate must be a number"
+
+def validate_input_age(POST):
+    """raises a value error if age is not an integer"""
+    if isinstance("user_age", int):
+        pass
+    else:
+        raise ValueError "Age must be an integer"
+
+
+
