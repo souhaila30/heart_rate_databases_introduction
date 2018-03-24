62aa8303d0727b63bfeb88bf0cee43bd279bae94
Souhaila Noor <souhaila@Souhailas-MacBook-Air.local>
2018-03-23 21:28:07 -0400

    tested input validation

--- test_main.py
+++ test_main.py
@@ -1,4 +1,7 @@
 from main import calculate_hr, check_tachycardia
+import pytest
+from validate import validate_input_email,validate_input_hr, validate_input_age
+
 def test_mean():
     data = [50,60,70,80,90]
     avg = calculate_hr(data)
@@ -9,4 +12,21 @@ def test_tachycardia():
     assert check_tachycardia(24,60) == "Tachycardia: False"
     assert check_tachycardia(100,112) == "Tachycardia: True"
 
+input1 = {"user.email": 10, "user.age": 24, "heart.rate":100}
+input2 = {"user.email":"san@duke.edu", "user.age":2.5, "heart.rate":80}
+input3 = {"user.email":"san@duke.edu", "user.age":10, "heart.rate":"80"}
 
+def test_age():
+    with pytest.raises(ValueError):
+        validate_input_age(input3)
+        validate_input_age(input2)
+
+def test_email():
+    with pytest.raises(TypeError):
+        validate_input_email(input1)
+        #validate_input_email(input2)
+
+def test_heart_rate():
+    with pytest.raises(ValueError):
+        validate_input_hr(input1)
+        validate_input_hr(input3)
